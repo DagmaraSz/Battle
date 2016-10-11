0247f431388426d4daa4446dc229a7832b3fdd49
Dagmara <daga.sz@hotmail.com>
2016-10-11 14:59:43 +0100

    PRGing

--- app.rb
+++ app.rb
@@ -1,19 +1,24 @@
 require 'sinatra/base'
-require 'shotgun'
 
 class Battle < Sinatra::Base
-  set :sessions, true
-  set :foo, 'bar'
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    p params
-    @Player1 = params[:Player1]
-    @Player2 = params[:Player2]
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player1 = session[:Player1]
+    @Player2 = session[:Player2]
     erb(:play)
   end
+
+
 run! if app_file == $0
 end
