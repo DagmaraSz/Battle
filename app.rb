c6d1a8e1e89c8aba8de22e7df5b8b14059107750
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-11 13:01:46 +0100

    Challenge 18 entering players' names

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
+require 'shotgun'
 
 class Battle < Sinatra::Base
   set :sessions, true
   set :foo, 'bar'
 
   get '/' do
-  
+    erb(:index)
   end
+
+  post '/names' do
+    p params
+    @Player1 = params[:Player1]
+    @Player2 = params[:Player2]
+    erb(:play)
+  end
+run! if app_file == $0
 end
---
+++ spec/features/players_names_spec.rb
@@ -0,0 +1,16 @@
+require '././app.rb'
+require 'spec_helper'
+
+
+RSpec.feature "players_names", :type => :feature do
+
+  scenario "Users type their name into a form" do
+    visit "/"
+
+    fill_in "Player1", :with => "Bob"
+    fill_in "Player2", :with => "Steve"
+    click_button "Start"
+
+    expect(page).to have_text("Players' names are Player1: Bob, Player2: Steve")
+  end
+end
